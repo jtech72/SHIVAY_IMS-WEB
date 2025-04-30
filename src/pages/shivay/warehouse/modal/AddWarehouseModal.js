97ed49c35cd5c317841dad4375e6629c4f00c25a
vaibhav <vaibhav34kahyap>
2025-04-30 18:37:29 +0530

    bugs fixed

--- src/pages/shivay/warehouse/modal/AddWarehouseModal.js
+++ src/pages/shivay/warehouse/modal/AddWarehouseModal.js
@@ -78,7 +78,7 @@ const AddWarehouseModal = ({ showModal, handleClose, warehouseData }) => {
                         <Col sm={6}>
                             <Form.Group className="mb-1">
                                 <Form.Label className='mb-0'>Warehouse <span className='text-danger'>*</span></Form.Label>
-                                <Form.Control
+                                  <Form.Control
                                     type="text"
                                     placeholder="Enter Warehouse"
                                     {...register('warehouse', {
